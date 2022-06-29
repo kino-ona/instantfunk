255d8ce61bf88abca21651cadb72527e17f0835f
dmlrpfl <dmlrpfl@gmail.com>
2022-06-29 13:19:04 +0900

    메인 작업중

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,30 @@
 $(document).ready(function(){
 	setTimeout(function(){
-		const visualSwiper = new Swiper('.visual .swiper-container', {
+		const video = $('.visual .swiper-container').find('.swiper-slide').find('video').get();
+		$('.visual .swiper-container').swiper({
 			pagination: '.swiper-pagination',
-			// paginationClickable: true,
-			// nextButton: '.visual .swiper-button-next',
-			// prevButton: '.visual .swiper-button-prev'
+      paginationType: 'fraction',
+			onInit: function(swiper) {
+				console.log(video);
+				video[swiper.activeIndex].play();
+			},
+			onSlideChangeStart: function(swiper) {
+				for (let i = 0; i >= video.length; i++) {
+					video[i].pause();
+				}
+			},
+			onSlideChangeEnd: function(swiper) {
+				video[swiper.activeIndex].play();
+			}
 		});
 	}, 800);
+
+	$('.best-menu .best-menu__item .swiper-container').swiper();
+
+	$('.celeb-curation .celeb-curation__swiper .swiper-container').swiper({
+		nextButton: '.celeb-curation .celeb-curation__swiper .swiper-button-next',
+		prevButton: '.celeb-curation .celeb-curation__swiper .swiper-button-prev'
+	});
 
 	const screenTarget = $('.celeb-curation__screen');
 	const screenTargetOffset = screenTarget.offset().top;
