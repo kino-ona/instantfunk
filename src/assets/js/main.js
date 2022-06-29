1ff6d9c2d4d00a3a8f42231db6ec5432cb918c01
dmlrpfl <dmlrpfl@gmail.com>
2022-06-29 18:05:08 +0900

    메인 작업중 비쥬얼 완료

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,20 +1,62 @@
 $(document).ready(function(){
 	setTimeout(function(){
-		const video = $('.visual .swiper-container').find('.swiper-slide').find('video').get();
-		$('.visual .swiper-container').swiper({
+		const visualSwiper = $('.visual .swiper-container').swiper({
+			speed: 600,
+			loop: true,
 			pagination: '.swiper-pagination',
       paginationType: 'fraction',
 			onInit: function(swiper) {
-				console.log(video);
+				const video = swiper.slides.find('video').get();
+				const target = $('.visual .visual__control').find('.visual__video');
+
 				video[swiper.activeIndex].play();
+
+				target.removeClass('visual__video--visible');
 			},
 			onSlideChangeStart: function(swiper) {
-				for (let i = 0; i >= video.length; i++) {
+				const video = swiper.slides.find('video').get();
+				const target = $('.visual .visual__control').find('.visual__video');
+				let realIndex = swiper.slides.eq(swiper.activeIndex).attr('data-swiper-slide-index') || swiper.activeIndex;
+
+				for (let i = 0; i < video.length; i++) {
 					video[i].pause();
+				}
+
+				target.removeClass('visual__video--visible');
+
+				if (Number(realIndex) + 1 == swiper.slides.length - 2) {
+					target.first().addClass('visual__video--visible');
+				} else {
+					target.eq(Number(realIndex) + 1).addClass('visual__video--visible');
 				}
 			},
 			onSlideChangeEnd: function(swiper) {
+				const video = swiper.slides.find('video').get();
+
 				video[swiper.activeIndex].play();
+			},
+		});
+
+		$('.visual .visual__control').on({
+			mouseenter: function() {
+				const target = $(this).find('.visual__video');
+				let realIndex = visualSwiper.slides.eq(visualSwiper.activeIndex).attr('data-swiper-slide-index') || visualSwiper.activeIndex;
+				
+				target.removeClass('visual__video--visible');
+
+				if (Number(realIndex) + 1 == visualSwiper.slides.length - 2) {
+					target.first().addClass('visual__video--visible');
+				} else {
+					target.eq(Number(realIndex) + 1).addClass('visual__video--visible');
+				}
+			},
+			mouseleave: function() {
+				const target = $(this).find('.visual__video');
+
+				target.removeClass('visual__video--visible');
+			},
+			click: function() {
+				visualSwiper.slideNext();
 			}
 		});
 	}, 800);
@@ -29,6 +71,7 @@ $(document).ready(function(){
 	const screenTarget = $('.celeb-curation__screen');
 	const screenTargetOffset = screenTarget.offset().top;
 	const screenTargetHeight = screenTarget.height();
+
 	$(window).scroll(function(){
 		let scrollTop = $(window).scrollTop();
 		if (scrollTop >= screenTargetOffset - screenTargetHeight) {
