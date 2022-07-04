9c3f9a5af9ec380c0eb62d59f9fb97266908587c
dlrpfl <dmlrpfl@gmail.com>
2022-07-04 17:15:10 +0900

    메인 비쥬얼 수정

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,9 +9,9 @@ $(document).ready(function(){
 		paginationType: 'fraction',
 		onInit: function(swiper) {
 			const video = swiper.slides.find('video').get();
-			const target = $('.visual .visual__control').find('.visual__video');
+			const target = $('.desktop .visual .visual__control').find('.visual__video');
 
-			swiper.slides.eq(swiper.activeIndex).find('.visual__contents').addClass('visual__contents--fadein')
+			swiper.slides.eq(swiper.activeIndex).find('.visual__contents').addClass('visual__contents--fadein');
 
 			video[swiper.activeIndex].play();
 
@@ -19,7 +19,7 @@ $(document).ready(function(){
 		},
 		onSlideChangeStart: function(swiper) {
 			const video = swiper.slides.find('video').get();
-			const target = $('.visual .visual__control').find('.visual__video');
+			const target = $('.desktop .visual .visual__control').find('.visual__video');
 			let realIndex = swiper.slides.eq(swiper.activeIndex).attr('data-swiper-slide-index') || swiper.activeIndex;
 
 			for (let i = 0; i < video.length; i++) {
@@ -41,8 +41,8 @@ $(document).ready(function(){
 		},
 	});
 
-	$('.visual .visual__control').on({
-		mouseenter: function() {
+	$(document).on('mouseenter mouseleave', '.desktop .visual .visual__control', function(event){
+		if (event.type === 'mouseenter') {
 			const target = $(this).find('.visual__video');
 			let realIndex = visualSwiper.slides.eq(visualSwiper.activeIndex).attr('data-swiper-slide-index') || visualSwiper.activeIndex;
 			
@@ -53,15 +53,16 @@ $(document).ready(function(){
 			} else {
 				target.eq(Number(realIndex) + 1).addClass('visual__video--visible');
 			}
-		},
-		mouseleave: function() {
+		}
+		if (event.type === 'mouseleave') {
 			const target = $(this).find('.visual__video');
 
 			target.removeClass('visual__video--visible');
-		},
-		click: function() {
-			visualSwiper.slideNext();
 		}
+	});
+
+	$('.visual .visual__control').on('click', function(){
+		visualSwiper.slideNext();
 	});
 
 	$('.best-menu .best-menu__item .swiper-container').swiper();
