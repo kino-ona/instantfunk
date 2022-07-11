0294aa4c47e391ff98881dd8ad2081c9326f44f8
dlrpfl <dmlrpfl@gmail.com>
2022-07-11 09:59:39 +0900

    상품 목록

--- src/web/assets/js/product.js
+++ src/web/assets/js/product.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
     var targetHeight = target.height();
     var winHeight = $(window).height();
 
-    if ($(window).scrollTop() > (targetOffset + targetHeight - winHeight - 62)) {
+    if ($(window).scrollTop() > (targetOffset + targetHeight - winHeight)) {
       target.find('.product-list__sort').addClass('product-list__sort--absoluted');
     } else {
       target.find('.product-list__sort').removeClass('product-list__sort--absoluted');
